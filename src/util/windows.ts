2bc898d378c471d7908f580de83f211beb4873c1
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2022-08-15 02:40:42 -0400

    Removed obsoelete patching functions

--- src/util/windows.ts
+++ src/util/windows.ts
@@ -1,11 +1,7 @@
-import {readFile, writeFile} from 'fs/promises';
+import {readFile} from 'fs/promises';
 import {TranscodeEncoding} from 'buffer';
 
-import {
-	checksumUpdate,
-	signatureGet,
-	signatureSet
-} from 'portable-executable-signature';
+import {signatureGet, signatureSet} from 'portable-executable-signature';
 import {NtExecutable, NtExecutableResource, Resource, Data} from 'resedit';
 
 import {align, bufferAlign, bufferToArrayBuffer, launcher, once} from '../util';
@@ -31,30 +27,6 @@ const IMAGE_SCN_MEM_SHARED = 0x10000000;
 const IMAGE_SCN_MEM_EXECUTE = 0x20000000;
 const IMAGE_SCN_MEM_READ = 0x40000000;
 const IMAGE_SCN_MEM_WRITE = 0x80000000;
-
-export interface IPeResourceReplace {
-	//
-	/**
-	 * Replace icons if not null.
-	 *
-	 * @default null
-	 */
-	iconData?: Readonly<Buffer> | null;
-
-	/**
-	 * Replace version strings if not null.
-	 *
-	 * @default null
-	 */
-	versionStrings?: Readonly<{[key: string]: string}> | null;
-
-	/**
-	 * If true remove signature if present.
-	 *
-	 * @default false
-	 */
-	removeSignature?: boolean | null;
-}
 
 export interface IPePatchProjector {
 	//
@@ -121,89 +93,6 @@ export function peVersionInts(version: string): [number, number] | null {
 }
 
 /**
- * Replace resources in Windows PE file.
- *
- * @param path File path.
- * @param options Replacement options.
- */
-export async function peResourceReplace(
-	path: string,
-	options: Readonly<IPeResourceReplace>
-) {
-	const {iconData, versionStrings, removeSignature} = options;
-
-	// Read EXE file and remove signature if present.
-	const exeOriginal = await readFile(path);
-	const signedData = removeSignature ? null : signatureGet(exeOriginal);
-	let exeData = signatureSet(exeOriginal, null, true, true);
-
-	// Parse resources.
-	const exe = NtExecutable.from(exeData);
-	const res = NtExecutableResource.from(exe);
-
-	// Replace all the icons in all icon groups.
-	if (iconData) {
-		const ico = Data.IconFile.from(bufferToArrayBuffer(iconData));
-		for (const iconGroup of Resource.IconGroupEntry.fromEntries(
-			res.entries
-		)) {
-			Resource.IconGroupEntry.replaceIconsForResource(
-				res.entries,
-				iconGroup.id,
-				iconGroup.lang,
-				ico.icons.map(icon => icon.data)
-			);
-		}
-	}
-
-	// Update strings if present for all the languages.
-	if (versionStrings) {
-		for (const versionInfo of Resource.VersionInfo.fromEntries(
-			res.entries
-		)) {
-			// Get all the languages, not just available languages.
-			const languages = versionInfo.getAllLanguagesForStringValues();
-			for (const language of languages) {
-				versionInfo.setStringValues(language, versionStrings);
-			}
-
-			// Update integer values from parsed strings if possible.
-			const {FileVersion, ProductVersion} = versionStrings;
-			if (FileVersion) {
-				const uints = peVersionInts(FileVersion);
-				if (uints) {
-					const [ms, ls] = uints;
-					versionInfo.fixedInfo.fileVersionMS = ms;
-					versionInfo.fixedInfo.fileVersionLS = ls;
-				}
-			}
-			if (ProductVersion) {
-				const uints = peVersionInts(ProductVersion);
-				if (uints) {
-					const [ms, ls] = uints;
-					versionInfo.fixedInfo.productVersionMS = ms;
-					versionInfo.fixedInfo.productVersionLS = ls;
-				}
-			}
-
-			versionInfo.outputToResourceEntries(res.entries);
-		}
-	}
-
-	// Update resources.
-	res.outputResource(exe);
-	exeData = exe.generate();
-
-	// Add back signature if not removing.
-	if (signedData) {
-		exeData = signatureSet(exeData, signedData, true, true);
-	}
-
-	// Write updated EXE file.
-	await writeFile(path, Buffer.from(exeData));
-}
-
-/**
  * Search data for string, yields indexes and strings.
  *
  * @param data Data to search.
@@ -239,24 +128,6 @@ function* dataStrings(
 			string
 		};
 	}
-}
-
-/**
- * List PE file sections.
- *
- * @param data PE data.
- * @returns PE sections.
- */
-function windowsPeSections(data: Readonly<Buffer>) {
-	return NtExecutable.from(bufferToArrayBuffer(data), {
-		ignoreCert: true
-	})
-		.getAllSections()
-		.map(({info}) => ({
-			name: info.name,
-			offset: info.pointerToRawData,
-			size: info.sizeOfRawData
-		}));
 }
 
 const patchesOutOfDateDisable32 = once(() => [
@@ -763,171 +634,6 @@ const patchesOutOfDateDisable64 = once(() => [
 		}
 	]
 ]);
-
-/**
- * Attempt to replace Windows window title by modifying the .rdata section.
- * Throws an error if replacement title larger than current title.
- *
- * @param data Projector data.
- * @param title Replacement title.
- * @returns Patched data or null if no patch found.
- */
-function windowsPatchWindowTitleRdata(data: Buffer, title: string) {
-	const enc = 'utf16le';
-
-	// Locate the rdata section in the buffer.
-	let rdataInfo = null;
-	for (const info of windowsPeSections(data)) {
-		if (info.name === '.rdata') {
-			rdataInfo = info;
-		}
-	}
-	if (!rdataInfo) {
-		return null;
-	}
-	const rdata = data.subarray(
-		rdataInfo.offset,
-		rdataInfo.offset + rdataInfo.size
-	);
-
-	// Search for known titles (only one format known).
-	let found: [number, number] | null = null;
-	for (const [pre, reg] of [
-		['Adobe Flash Player ', /^Adobe Flash Player \d+([.,]\d+)*$/]
-	] as [string, RegExp][]) {
-		for (const {index, string, data} of dataStrings(rdata, pre, enc, reg)) {
-			// Only one match expected.
-			if (found) {
-				throw new Error(
-					`Multiple window titles found: ${JSON.stringify(string)}`
-				);
-			}
-
-			// Check if title can replace string.
-			if (title.length > string.length) {
-				throw new Error(
-					`Replacement window title longer that ${string.length}`
-				);
-			}
-
-			// Remember index for later.
-			found = [index, data.length];
-		}
-
-		// Stop searching once a known title is found.
-		if (found) {
-			break;
-		}
-	}
-
-	// If no match found, no title to replace.
-	if (!found) {
-		return null;
-	}
-
-	// Replace title.
-	const replacingO = rdataInfo.offset + found[0];
-	const replacing = data.subarray(replacingO, replacingO + found[1]);
-	replacing.fill(0);
-	replacing.write(title, enc);
-
-	// Update checksum.
-	checksumUpdate(data, true);
-	return data;
-}
-
-/**
- * Attempt to replace Windows window title by modifying the resources.
- *
- * @param data Projector data.
- * @param title Replacement title.
- * @returns Patched data or null if no patch found.
- */
-function windowsPatchWindowTitleRsrc(data: Buffer, title: string) {
-	// Read EXE file and remove signature if present.
-	const signedData = signatureGet(data);
-	let exeData = signatureSet(data, null, true, true);
-
-	// Parse resources.
-	const exe = NtExecutable.from(exeData);
-	const res = NtExecutableResource.from(exe);
-
-	// Match all known titles.
-	const titleMatch =
-		/^((Shockwave )?Flash|(Adobe|Macromedia) Flash Player \d+([.,]\d+)*)$/;
-
-	// Find ID of string table with the title and ID of title if present.
-	const typeStringTable = 6;
-	let titleStringTableId = null;
-	let titleStringTableEntryId = null;
-	for (const entry of res.entries) {
-		if (entry.type !== typeStringTable) {
-			continue;
-		}
-		const table = Resource.StringTable.fromEntries(entry.lang, [entry]);
-		for (const {text} of table.getAllStrings()) {
-			if (text.startsWith('Projector ')) {
-				titleStringTableId = entry.id;
-				break;
-			}
-		}
-		if (titleStringTableId === null) {
-			continue;
-		}
-		for (const {id, text} of table.getAllStrings()) {
-			if (titleMatch.test(text)) {
-				titleStringTableEntryId = id;
-				break;
-			}
-		}
-		break;
-	}
-	if (titleStringTableId === null || titleStringTableEntryId === null) {
-		return null;
-	}
-
-	// Replace all the entries.
-	for (const entry of res.entries) {
-		if (entry.type !== typeStringTable || entry.id !== titleStringTableId) {
-			continue;
-		}
-		const table = Resource.StringTable.fromEntries(entry.lang, [entry]);
-		table.setById(titleStringTableEntryId, title);
-		table.replaceStringEntriesForExecutable(res);
-	}
-
-	// Update resources.
-	res.outputResource(exe);
-	exeData = exe.generate();
-
-	// Add back signature if not removing.
-	if (signedData) {
-		exeData = signatureSet(exeData, signedData, true, true);
-	}
-
-	return Buffer.from(exeData);
-}
-
-/**
- * Attempt to replace Windows window title.
- *
- * @param data Projector data.
- * @param title Replacement title.
- * @returns Patched data, can be same buffer, but modified.
- */
-export function windowsPatchWindowTitle(data: Buffer, title: string) {
-	const patchRdata = windowsPatchWindowTitleRdata(data, title);
-	if (patchRdata) {
-		return patchRdata;
-	}
-
-	const patchResource = windowsPatchWindowTitleRsrc(data, title);
-	if (patchResource) {
-		return patchResource;
-	}
-
-	throw new Error('Failed to patch the window title');
-}
 
 /**
  * Get the EXE section that includes an address.
