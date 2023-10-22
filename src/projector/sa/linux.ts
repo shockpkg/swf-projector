b57d2c9804fa8dd7b720f00680d6f6448d0b704b
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2023-10-21 23:08:33 -0400

    Migrated Linux modifications over to patches

--- src/projector/sa/linux.ts
+++ src/projector/sa/linux.ts
@@ -1,15 +1,17 @@
-import {open, readFile, stat, writeFile} from 'node:fs/promises';
+import {mkdir, open, stat, writeFile} from 'node:fs/promises';
 import {basename, dirname} from 'node:path';
 
 import {
 	ArchiveDir,
+	Entry,
 	PathType,
 	createArchiveByFileExtensionOrThrow
 } from '@shockpkg/archive-files';
 
 import {linuxProjectorPatch} from '../../util/linux';
 import {EM_X86_64} from '../../util/internal/linux/elf';
-import {ProjectorSa} from '../sa';
+import {IFilePatch, ProjectorSa} from '../sa';
+import {concat} from '../../util/internal/data';
 
 /**
  * ProjectorSaLinux object.
@@ -113,6 +115,51 @@ export class ProjectorSaLinux extends ProjectorSa {
 				);
 		}
 
+		const patches = await this._getPatches();
+
+		/**
+		 * Extract entry, and also apply patches if any.
+		 *
+		 * @param entry Archive entry.
+		 * @param dest Output path.
+		 */
+		const extract = async (entry: Entry, dest: string) => {
+			if (entry.type === PathType.FILE) {
+				let data: Uint8Array | null = null;
+				for (const patch of patches) {
+					if (patch.match(entry.volumePath)) {
+						if (!data) {
+							// eslint-disable-next-line no-await-in-loop
+							const d = await entry.read();
+							if (!d) {
+								throw new Error(
+									`Failed to read: ${entry.volumePath}`
+								);
+							}
+							data = new Uint8Array(
+								d.buffer,
+								d.byteOffset,
+								d.byteLength
+							);
+						}
+						// eslint-disable-next-line no-await-in-loop
+						data = await patch.modify(data);
+					}
+				}
+
+				if (data) {
+					await mkdir(dirname(dest), {recursive: true});
+					await writeFile(dest, data);
+					await entry.setAttributes(dest, null, {
+						ignoreTimes: true
+					});
+					return;
+				}
+			}
+
+			await entry.extract(dest);
+		};
+
 		let playerPath = '';
 		await archive.read(async entry => {
 			const {volumePath, type, mode} = entry;
@@ -132,64 +179,123 @@ export class ProjectorSaLinux extends ProjectorSa {
 			}
 			playerPath = volumePath;
 
-			await entry.extract(path);
+			await extract(entry, path);
 			return true;
 		});
 
 		if (!playerPath) {
 			throw new Error(`Failed to locate player in: ${player}`);
 		}
+
+		await Promise.all(patches.map(async p => p.after()));
 	}
 
 	/**
 	 * @inheritDoc
 	 */
-	protected async _modifyPlayer() {
+	protected async _modifyPlayer() {}
+
+	/**
+	 * Get patches to apply.
+	 *
+	 * @returns Patches list.
+	 */
+	protected async _getPatches() {
+		return (
+			await Promise.all([this._getPatchBinary(), this._getPatchMovie()])
+		).filter(p => p) as IFilePatch[];
+	}
+
+	/**
+	 * Get patch for binary.
+	 *
+	 * @returns Patch spec.
+	 */
+	// eslint-disable-next-line @typescript-eslint/require-await
+	protected async _getPatchBinary() {
 		const {
-			path,
 			patchWindowTitle,
 			patchMenuRemove,
 			patchProjectorPath,
 			patchProjectorOffset
 		} = this;
-		const movieData = await this.getMovieData();
-
-		let data: Uint8Array | null = null;
-
 		if (
-			patchWindowTitle !== null ||
-			patchMenuRemove ||
-			patchProjectorPath ||
-			patchProjectorOffset
-		) {
-			data = data || (await readFile(path));
-			data = linuxProjectorPatch(data, {
-				patchWindowTitle,
-				patchMenuRemove,
-				patchProjectorPath,
+			!(
+				patchWindowTitle !== null ||
+				patchMenuRemove ||
+				patchProjectorPath ||
 				patchProjectorOffset
-			});
+			)
+		) {
+			return null;
 		}
 
-		if (movieData) {
-			data = data || (await readFile(path));
-			const v = new DataView(
-				data.buffer,
-				data.byteOffset,
-				data.byteLength
-			);
-			const e = this._encodeMovieData(
-				movieData,
-				v.getUint16(18, true) === EM_X86_64 ? 'lid' : 'smd'
-			);
-			const d = new Uint8Array(data.length + e.length);
-			d.set(data);
-			d.set(e, data.length);
-			data = d;
+		const patch: IFilePatch = {
+			/**
+			 * @inheritdoc
+			 */
+			match: (file: string) => true,
+
+			/**
+			 * @inheritdoc
+			 */
+			modify: (data: Uint8Array) =>
+				linuxProjectorPatch(data, {
+					patchWindowTitle,
+					patchMenuRemove,
+					patchProjectorPath,
+					patchProjectorOffset
+				}),
+
+			/**
+			 * @inheritdoc
+			 */
+			after: () => {}
+		};
+		return patch;
+	}
+
+	/**
+	 * Get patch for movie.
+	 *
+	 * @returns Patch spec.
+	 */
+	// eslint-disable-next-line @typescript-eslint/require-await
+	protected async _getPatchMovie() {
+		const movieData = await this.getMovieData();
+		if (!movieData) {
+			return null;
 		}
 
-		if (data) {
-			await writeFile(path, data);
-		}
+		const patch: IFilePatch = {
+			/**
+			 * @inheritdoc
+			 */
+			match: (file: string) => true,
+
+			/**
+			 * @inheritdoc
+			 */
+			modify: (data: Uint8Array) => {
+				const v = new DataView(
+					data.buffer,
+					data.byteOffset,
+					data.byteLength
+				);
+				return concat([
+					data,
+					this._encodeMovieData(
+						movieData,
+						v.getUint16(18, true) === EM_X86_64 ? 'lid' : 'smd'
+					)
+				]);
+			},
+
+			/**
+			 * @inheritdoc
+			 */
+			after: () => {}
+		};
+		return patch;
 	}
 }
